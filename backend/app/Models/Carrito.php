dfcc738e2d5b55baa1f1736f9617e2632a312eb6
jhan-97 <lozanoclaros@outlook.com>
2025-05-07 19:53:22 -0500

    Update Carrito.php

--- backend/app/Models/Carrito.php
+++ backend/app/Models/Carrito.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Carrito extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = "carrito"; 
+    protected $primaryKey = "idCarrito";
+
+    protected $fillable = [
+        "idCarrito",
+        "idUsuario", 
+        "estado"     
+    ];
+
+    public function productos()
+    {
+        // Relación de muchos a muchos
+        return $this->belongsToMany(Producto::class, 'carrito_producto', 'idCarrito', 'idProducto')
+                    ->withPivot('cantidad'); 
+    }
+
+    public function usuario()
+    {
+        // Relación con usuario
+        return $this->belongsTo(Usuario::class, "idUsuario");
+    }
 }
